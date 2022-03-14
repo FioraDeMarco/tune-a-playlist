abcb321e57dc5401eca5caaa948cef639c74b1a8
Fiora De Marco <fiorademarco7@gmail.com>
2022-03-13 21:48:10 -0400

    late attempts on real data party in sj

--- Desktop/Fullstack/Senior/soundaathon/src/App.js
+++ Desktop/Fullstack/Senior/soundaathon/src/App.js
@@ -5,6 +5,7 @@ import useGeoLocation from "./useGeoLocation";
 import React, { useState, useEffect, Component } from "react";
 import Map from "./Map";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 //import axios, { Axios } from "axios";
 // const express = require("express");
@@ -17,36 +18,55 @@ class App extends Component {
     super(props);
     this.state = {
       items: [],
-      isLoaded: false,
+      // isLoaded: false,
     };
   }
 
-  //app.use(cors())
   componentDidMount() {
-    // fetch("https://api.seatgeek.com/2/venues");
-    // fetch("https://www.eventbrite.com/oauth/authorize?");
-    // res.header("Access-Control-Allow-Origin", "true")
-
-    fetch(
-      "https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=6XSP4NDBIQGSVUDHGB&redirect_uri=http://localhost:3000/app",
-      // { mode: "no-cors" }
-      {
-        mode: "no-cors",
-        headers: {
-          authorization: "NYHFH3VPU5IRSEOF2WR4",
-          // "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        this.setState({
-          isLoaded: true,
-          items: json,
-        });
+    /* fetches the necessary jsonblob information to be outputted in read.js in order to acquire the necessary data for the movies to be outputted to the screen at the localhost port specified below in the url. An error message to pop up in case something goes wrong. */
+    axios
+      .get("http://localhost:3000/app")
+      .then((response) => {
+        this.setState({ items: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    //   // axios.get()
   }
+
+  // componentDidMount() {
+  //   // fetch("https://api.seatgeek.com/2/venues");
+  //   // fetch("https://www.eventbrite.com/oauth/authorize?");_
+  //   // res.header("Access-Control-Allow-Origin", "true")
+  //   // const cors_api_host = 'cors-anywhere.herokuapp.com'
+  //   fetch(
+  //     "https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=2YCSYBBPC5NKYEV3QLATK3NYETYIGQZSZRLHNAL5USKK6Q3BGR",
+  //     {
+  //       // mode: "no-cors",
+  //       headers: {
+  //         authorization: "NYHFH3VPU5IRSEOF2WR4",
+  //         "Access-Control-Allow-Origin": true,
+  //       },
+  //     }
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       this.setState({
+  //         isLoaded: true,
+  //         items: json,
+  //       });
+  //     });
+  // }
+
+  //https://www.eventbrite.com/platform/api/"
+  // async componentDidMount() {
+  //   const myHeader =new Headers ('Access-Control-Allow-Origin", "true')
+  //   const url =
+  //     "https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=2YCSYBBPC5NKYEV3QLATK3NYETYIGQZSZRLHNAL5USKK6Q3BGR&redirect_uri=/http://localhost:3000";
+  //   const response = await fetch(url, { mode: "no-cors" });
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
   render() {
     const { items } = this.state;
     //const { isLoaded, items } = this.state;
@@ -73,5 +93,4 @@ class App extends Component {
   }
 }
 //}
-
 export default App;
